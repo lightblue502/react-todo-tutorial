ade78798eefb4a14c64365882dabbe0b856379fa
new <new@pupasoft.com>
2017-11-23 23:13:05 +0700

    todo-v7-validate

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,13 +1,42 @@
 import React from 'react'
+import _ from 'lodash'
 
 export default class TodoCreate extends React.Component{
+
+    state={
+        error: null
+    }
+
+    renderError(){
+        return this.state.error && <div style={{color: 'red'}}>
+            {this.state.error}
+        </div>
+    }
 
     handleCreate(event){
         event.preventDefault(); //stop event default
         const createInput = this.createInput; //want bind this in handleCreate
         const task = this.createInput.value; 
+
+        const validateInput = this.validateInput(task);
+
+        if(validateInput){
+            this.setState({ error: validateInput});
+            return;
+        }
+        this.setState({ error: null });
         this.props.createTask(task);
         this.createInput.value = ""
+    }
+
+    validateInput(task){
+        if (!task) {
+            return 'Please enter a task.';
+        } else if (_.find(this.props.todos, todo => todo.task === task)) {
+            return 'Task already exists.';
+        } else {
+            return null;
+        }
     }
 
     render(){
@@ -18,6 +47,7 @@ export default class TodoCreate extends React.Component{
                     ref={instance => this.createInput = instance} 
                     placeholder="What do I need to do?" />
                 <button> Create </button>
+                { this.renderError() }
             </form>
         )
     }
